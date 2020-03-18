dcea95c10ababf20f7ab78e8c1010d2b1f611c39
henrry <henrrywelson@outlook.com>
2020-03-18 09:18:44 +0800

    页面组件拆分

--- vue.config.js
+++ vue.config.js
@@ -2,10 +2,10 @@ module.exports = {
   publicPath: '',
   devServer: {
     host: 'localhost',
-    port: 9090,
+    port: 8080,
     proxy: {
       '/mock': {
-        target: 'http://localhost:9090',
+        target: 'http://localhost:8080',
         // 是否开启 websocket
         ws: false,
         // 是否将主机头的原点改为目标的URL
